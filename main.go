8957b0c57a44b3b067117aae0fe8f0bf4f58155d
santiago-rodrig <santo1996.29@gmail.com>
2021-03-03 21:12:11 -0400

    complete the program

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/santiago-rodrig/tempconv"
+)
+
 func main() {
-	println("not implemented")
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cfgo: %v\n", err)
+			os.Exit(1)
+		}
+
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
 }
